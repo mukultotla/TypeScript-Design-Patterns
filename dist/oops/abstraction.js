061388686ffabac23714ebe1226d14b3d30f83df
Mukul Totla <mukultotla4@gmail.com>
2025-01-14 15:06:39 +0530

    abstraction implementation

---
+++ dist/oops/abstraction.js
@@ -0,0 +1,28 @@
+"use strict";
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    area() {
+        return Math.PI * this.radius * this.radius;
+    }
+    perimeter() {
+        return 2 * Math.PI * this.radius;
+    }
+}
+class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    area() {
+        return this.width * this.height;
+    }
+    perimeter() {
+        return 2 * (this.width + this.height);
+    }
+}
+let circle = new Circle(5);
+let rectangle = new Rectangle(3, 4);
+console.log("Area of Circle -> ", circle.area());
+console.log("Perimeter of Rectangle -> ", rectangle.perimeter());
---
+++ src/oops/abstraction.ts
@@ -0,0 +1,30 @@
+interface Shape {
+  area(): number;
+  perimeter(): number;
+}
+
+class Circle implements Shape {
+  constructor(private radius: number) {}
+  area(): number {
+    return Math.PI * this.radius * this.radius;
+  }
+  perimeter(): number {
+    return 2 * Math.PI * this.radius;
+  }
+}
+
+class Rectangle implements Shape {
+  constructor(private width: number, private height: number) {}
+  area(): number {
+    return this.width * this.height;
+  }
+  perimeter(): number {
+    return 2 * (this.width + this.height);
+  }
+}
+
+let circle: Circle = new Circle(5);
+let rectangle: Rectangle = new Rectangle(3, 4);
+
+console.log("Area of Circle -> ", circle.area());
+console.log("Perimeter of Rectangle -> ", rectangle.perimeter());
